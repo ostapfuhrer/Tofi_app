764dea7b9924fed41ac33c4df84bab764b070c70
Ostap Kresan <ostap_kresan@epam.com>
2024-10-11 15:13:08 +0300

    minor refactoring of composable elements

--- app/src/main/java/com/example/tofi_app/goal_screen/screen/GoalScreen.kt
+++ app/src/main/java/com/example/tofi_app/goal_screen/screen/GoalScreen.kt
@@ -9,22 +9,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.tofi_app.R
 import com.example.tofi_app.common.composable.IndicatorRow
+import com.example.tofi_app.common.composable.IntroScreenHeader
 import com.example.tofi_app.common.composable.LargeButton
 import com.example.tofi_app.goal_screen.viewmodel.GoalScreenViewModel
 import com.example.tofi_app.ui.theme.AppColors
@@ -53,28 +47,10 @@ fun GoalScreenUi() {
             IndicatorRow(indicators = indicators)
         }
         Spacer(modifier = Modifier.height(120.dp))
-        Icon(
-            painter = painterResource(id = R.drawable.ic_goal),
-            contentDescription = null,
-            modifier = Modifier
-                .size(64.dp)
-                .align(Alignment.CenterHorizontally)
-        )
-        Text(
-            text = "What your Goal?",
-            style = MaterialTheme.typography.bodySmall.copy(fontWeight = FontWeight.Bold),
-            fontSize = 24.sp,
-            modifier = Modifier
-                .padding(top = 8.dp)
-                .align(Alignment.CenterHorizontally),
-        )
-        Text(
-            text = "we will help you choose calories for your goals",
-            style = MaterialTheme.typography.bodySmall,
-            fontSize = 14.sp,
-            modifier = Modifier
-                .padding(bottom = 16.dp, top = 8.dp)
-                .align(Alignment.CenterHorizontally),
+        IntroScreenHeader(
+            iconId = R.drawable.ic_goal,
+            title = "What your Goal?",
+            description = "We will help you choose calories for your goals"
         )
         Spacer(modifier = Modifier.weight(1f))
 
