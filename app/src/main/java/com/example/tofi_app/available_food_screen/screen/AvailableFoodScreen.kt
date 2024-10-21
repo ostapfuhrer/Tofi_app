eccc5e595aae7f6e0d052bfc993a760ea20d99bd
Ostap Kresan <ostap_kresan@epam.com>
2024-10-21 12:31:03 +0300

    ActivityLevelScreen.kt delete unused imports

--- app/src/main/java/com/example/tofi_app/available_food_screen/screen/AvailableFoodScreen.kt
+++ app/src/main/java/com/example/tofi_app/available_food_screen/screen/AvailableFoodScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,8 +14,6 @@ import androidx.compose.ui.unit.dp
 import com.example.tofi_app.R
 import com.example.tofi_app.common.composable.IndicatorRow
 import com.example.tofi_app.common.composable.IntroScreenHeader
-import com.example.tofi_app.common.composable.LargeButton
-import com.example.tofi_app.ui.theme.AppColors
 
 @Composable
 fun AvailableFoodScreen() {
