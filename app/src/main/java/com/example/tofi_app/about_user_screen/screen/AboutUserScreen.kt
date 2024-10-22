110ba368825149f9677b3d20cd7b77cc4c6b8e74
Ostap Kresan <ostap_kresan@epam.com>
2024-10-22 13:10:37 +0300

    AvailableFoodScreen.kt impl UI

--- app/src/main/java/com/example/tofi_app/about_user_screen/screen/AboutUserScreen.kt
+++ app/src/main/java/com/example/tofi_app/about_user_screen/screen/AboutUserScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -40,7 +39,7 @@ fun AboutUserScreenUi() {
     ) {
         val indicators = listOf(true, false, false, false, false, false, false)
         IndicatorRow(indicators = indicators)
-        
+
         IntroScreenHeader(
             modifier = Modifier.padding(top = 120.dp),
             iconId = R.drawable.ic_about,
