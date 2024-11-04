847e684d49626c418249f0b406c70c7b9249c7bf
Ostap Kresan <ostapkresan@gmail.com>
2024-11-04 17:02:02 +0200

    add Utils for Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.example.tofi_app"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.tofi_app"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
